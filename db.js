58e404c7e432bf6460a49cff0d6297e94948b8d7
Ekaterina Nichiporenko <ekaterinanichiporenko@Ekaterinas-MacBook-Air.local>
2023-07-26 14:44:49 +0200

    added procfile

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
-  password: "fooCoding321",
+  password: "rooT123!4",
   database: "my_profile",
   port: 3306,
+});
+connection.query('SELECT 1 + 1 AS result', (error, results, fields) => {
+  if (error) {
+    console.error('Ошибка выполнения запроса:', error);
+  } else {
+    console.log('Результат:', results[0].result);
+  }
 });
 module.exports = connection;
