999cfad7e15a371a589b71b750fa095548741395
Ekaterina Nichiporenko <ekaterinanichiporenko@Ekaterinas-MacBook-Air.local>
2023-07-27 16:46:50 +0200

    added pool

--- db.js
+++ db.js
@@ -23,17 +23,11 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  idleTimeout: 60000, 
   queueLimit: 0,
   enableKeepAlive: true,
   keepAliveInitialDelay: 0
 });
-connection.query('SELECT 1 + 1 AS result', (error, results, fields) => {
-  if (error) {
-    console.error( error);
-  } else {
-    console.log(results[0].result);
-  }
-});
+
 //module.exports = connection;
 module.exports = pool;
